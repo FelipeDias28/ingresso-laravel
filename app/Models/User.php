1fda4355c0d0da0e0c3f180dc0abafcb5bde9700
Felipe Dias <adfelipealmeida@gmail.com>
2023-03-01 10:49:45 -0300

    feat(relashionships): add relationships in Modls, 1:1 between User and Type_user and N:N between User and Event

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function typeUser()
+    {
+        return $this->hasOne(TypeUser::class);
+    }
+
+    public function events()
+    {
+        return $this->belongsToMany(Event::class, 'tickets');
+    }
 }
